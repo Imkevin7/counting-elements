d43afb8cac0e649d8f87002dd158eb9990300808
Kevin <kevin.pichette@gmail.com>
2014-09-17 23:23:16 -0500

    a

---
+++ js/controls.js
@@ -0,0 +1,10 @@
+function work(){
+      var a = $('div').length;
+       $('#divs').text(a);
+
+       var p = $('p').length;
+       $('#ps').text(p);
+
+       var h = $('h3').length;
+       $('#h').text(h);
+}
